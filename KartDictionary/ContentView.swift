dea5dc03dba1e019ba27363a697546999164ec52
Gold-MineScore <96890040+Gold-MineScore@users.noreply.github.com>
2022-04-29 15:11:29 +0900

    프로젝트 : 최초생성

--- KartDictionary/ContentView.swift
+++ KartDictionary/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        enum Tabs: Text{
+            case league = "리그"
+            case track = "트랙"
+            case kart = "카트바디"
+            case team = "선수/팀"
+            case record = "기록실"
+        }
+        
+        @State var tabSelection:Tabs = .league
+        
+        NavigationView {
+            TabView(selection:$tabSelection) {
+                Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+                Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+            }
+        }
     }
 }
 
