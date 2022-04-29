d51b69fd1bd719159bef886d82d356b3f3147497
Gold-MineScore <96890040+Gold-MineScore@users.noreply.github.com>
2022-04-29 14:28:53 +0900

    Initial Commit

---
+++ KartDictionary/KartDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  KartDictionaryApp.swift
+//  KartDictionary
+//
+//  Created by HWANG-C-K on 2022/04/29.
+//
+
+import SwiftUI
+
+@main
+struct KartDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
